291bc6e4a74d0186b8cdfc828c8157baa785c442
Alkarias <alkarias29@gmail.com>
2022-07-14 17:25:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    const list = process.argv.slice(2);      
+    console.log(list);
+}
+
+// Function call to initialize app
+init();
